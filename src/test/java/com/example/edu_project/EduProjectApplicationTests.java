ffd00c5cc047fa80b9725e33f6b1563ebc0c613c
tygras <tygras.mos@gmail.com>
2022-11-25 10:13:40 +0300

    Create new project

---
+++ src/test/java/com/example/edu_project/EduProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.edu_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
